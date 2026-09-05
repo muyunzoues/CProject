40997b8de66558ea54fd11de5c34403deee75215
LQZ <2058096897@qq.com>
2026-09-05 15:09:16 +0800

    首次提交

---
+++ TEST/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include "sum.h"
+using namespace std;
+
+int main()
+{
+    int a=5, b=10;
+    cout<<"Sum of "<<a<<" and "<<b<<" is: "<<sum(a,b)<<endl;
+    return 0;
+}
---
+++ TEST/sum.h
@@ -0,0 +1,7 @@
+#ifndef SUM_H
+#define SUM_H
+
+// 函数声明：告诉编译器 sum 函数长什么样
+int sum(int a, int b);
+
+#endif
